ae52a0bc0b10f278ab8f1b8a1d7d66b5e8337858
Justijn Depover <justijndepover@gmail.com>
2022-09-07 08:51:04 +0200

    initialise phpunit

---
+++ tests/VatCheckerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Justijndepover\SimpleVatChecker\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class VatCheckerTest extends TestCase
+{
+    /** @test */
+    public function it_asserts_true()
+    {
+        $this->assertTrue(true);
+    }
+}
